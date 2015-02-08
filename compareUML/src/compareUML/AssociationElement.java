331593611fa52e5593b76919f18e43b4c3cc771e
Xibo Wang <paulop5288@gmail.com>
2015-02-08 22:48:26 +0000

    don't revert

---
+++ compareUML/src/compareUML/AssociationElement.java
@@ -0,0 +1,28 @@
+package compareUML;
+
+public class AssociationElement extends PackagedElement{
+	protected String firstMemberEnd;
+	protected String secondMemberEnd;
+	public AssociationElement(String id, String firstMember, String secondMember) {
+		// TODO Auto-generated constructor stub\
+		super("Association", id);
+		this.firstMemberEnd = firstMember;
+		this.secondMemberEnd = secondMember;
+	}
+
+	
+	public String getFirstMemberEnd() {
+		return firstMemberEnd;
+	}
+	
+	public String getSecondMemberEnd() {
+		return secondMemberEnd;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString() + " 1st M : " + firstMemberEnd
+				+ " 2nd M : " + secondMemberEnd;
+	}
+}
