fbaa713fdb56e617170aa9c6ac1c87ec34fa3d70
Xibo Wang <paulop5288@gmail.com>
2015-02-08 20:24:17 +0000

    I created a class named CompareUML.

---
+++ compareUML/src/compareUML/CompareUML.java
@@ -0,0 +1,12 @@
+package compareUML;
+
+public class CompareUML {
+
+	public CompareUML() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("I created a new class");
+	}
+
+}
