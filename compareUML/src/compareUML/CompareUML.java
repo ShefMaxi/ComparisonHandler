62fa852c0bd3798eb5aa1bd02e857559116123d8
Xibo Wang <paulop5288@gmail.com>
2015-02-08 22:04:58 +0000

    no change

--- compareUML/src/compareUML/CompareUML.java
+++ compareUML/src/compareUML/CompareUML.java
@@ -5,8 +5,5 @@ public class CompareUML {
 	public CompareUML() {
 		// TODO Auto-generated constructor stub
 	}
-	public static void main(String[] args) {
-		System.out.println("I created a new class");
-	}
-// comment out
-}//hahahaha
+
+}
