a0977328607e2a13f91b99991a0cb5f22d8b3a87
Xibo Wang <paulop5288@gmail.com>
2015-02-09 15:45:15 +0000

    Create preprocess class

---
+++ compareUML/src/compareUML/PreprocessElements.java
@@ -0,0 +1,14 @@
+package compareUML;
+/*
+ * This class is to preprocess the arraylist created by
+ * xmi parser
+ */
+public class PreprocessElements {
+
+	public PreprocessElements() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
